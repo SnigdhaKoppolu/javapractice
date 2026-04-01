d6ff2302ad4c9d067a78b5d64ac9ff2f602a8e4c
snigdha <snigdhask27@gmail.com>
2026-04-01 22:03:19 +0530

    add looping statements(for, while, do while)

---
+++ src/com/loopingstatements/ForLoop.java
@@ -0,0 +1,21 @@
+package com.loopingstatements;
+
+public class ForLoop {
+
+	public static void main(String[] args) {
+		/*for(int i=0; i<=5; i++) {
+			System.out.println("Iteration:" +i);
+		}
+         System.out.println("selenium");
+	    */
+		
+		//for loop without curly braces
+		for(int i=1; i<=5; i++)
+			System.out.println("Hello world");
+	        System.out.println("java");
+	        System.out.println("selenium");
+	        
+	      
+	}
+
+}
---
+++ src/com/loopingstatements/ForLoopPrintName.java
@@ -0,0 +1,10 @@
+package com.loopingstatements;
+
+public class ForLoopPrintName {
+
+	public static void main(String[] args) {
+		for(int i=1; i<=10; i++) {
+			System.out.println("Happy Birthday");
+		}
+	}		
+	}
